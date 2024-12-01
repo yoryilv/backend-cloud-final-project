4fb7ac05994af402aae0de7d174d3926a48ec153
yoryi <j_leandro_vaskez@hotmail.com>
2024-12-01 09:41:45 -0500

    sasa

--- api-usuarios/CrearUsuario.py
+++ api-usuarios/CrearUsuario.py
@@ -10,72 +10,62 @@ def hash_password(password):
 # Función que maneja el registro de user y validación del password
 def lambda_handler(event, context):
     try:
-        # Obtener el email y el password
-        cinema_id = event.get('cinema_id')
-        user_id = event.get('user_id')
-        password = event.get('password')
-        role = event.get('role', 'client')
-        
-        # Verificar que el email y el password existen
-        if user_id and password:
-            
-            # Verificar que el rol sea válido
-            valid_roles = ['client', 'admin']
-            if role not in valid_roles:
+        # Obtener el user y el password
+        body = json.loads(event.get('body', '{}'))  # Asegúrate de cargar el body JSON
+        cinema_id = body.get('cinema_id')
+        user_id = body.get('user_id')
+        password = body.get('password')
+        role = body.get('role', 'client')
+
+        # Validar campos obligatorios
+        required_fields = ['user_id', 'cinema_id', 'password']
+        for field in required_fields:
+            if not body.get(field):
                 return {
+                    'statusCode': 400,
+                    'body': json.dumps({'error': f'Falta el campo obligatorio: {field}'})
+                }
+
+        # Verificar que el rol sea válido
+        valid_roles = ['client', 'admin']
+        if role not in valid_roles:
+            return {
                 'statusCode': 400,
                 'body': json.dumps({'error': 'Rol Invalido. Debe ser "client" o "admin"'})
-                }
-            
-            # Hashea la contraseña antes de almacenarla
-            hashed_password = hash_password(password)
-            
-            # Conectar DynamoDB
-            dynamodb = boto3.resource('dynamodb')
-            t_usuarios = dynamodb.Table('t_usuarios')
-            
-            # Verificar si el usuario ya existe
-            existing_user = t_usuarios.get_item(Key={'user_id': user_id})
-            if 'Item' in existing_user:
-                return {
-                'statusCode': 409,
-                'body': json.dumps({'error': 'User already exists'})
-                }
-            
-            # Almacena los datos del user en la tabla de usuarios en DynamoDB
-            t_usuarios.put_item(
-                Item={
-                    'cinema_id': cinema_id,
-                    'user_id': user_id,
-                    'password': hashed_password,
-                    'role': role
-                }
-            )
-            # Retornar un código de estado HTTP 200 (OK) y un mensaje de éxito
-            mensaje = {
-                'message': 'User registered successfully',
-                'user_id': user_id
-            }
-            return {
-                'statusCode': 200,
-                'body': mensaje
-            }
-        else:
-            mensaje = {
-                'error': 'Invalid request body: missing user_id or password'
-            }
-            return {
-                'statusCode': 400,
-                'body': mensaje
             }
 
+        # Hashea la contraseña antes de almacenarla
+        hashed_password = hash_password(password)
+
+        # Conectar DynamoDB
+        dynamodb = boto3.resource('dynamodb')
+        t_usuarios = dynamodb.Table('t_usuarios')
+
+        # Verificar si el usuario ya existe
+        existing_user = t_usuarios.get_item(Key={'user_id': user_id})
+        if 'Item' in existing_user:
+            return {
+                'statusCode': 409,
+                'body': json.dumps({'error': 'User already exists'})
+            }
+
+        # Almacena los datos del user en la tabla de usuarios en DynamoDB
+        t_usuarios.put_item(
+            Item={
+                'cinema_id': cinema_id,
+                'user_id': user_id,
+                'password': hashed_password,
+                'role': role
+            }
+        )
+
+        return {
+            'statusCode': 201,
+            'body': json.dumps({'message': 'Usuario creado correctamente'})
+        }
+    
     except Exception as e:
-        # Excepción y retornar un código de error HTTP 500
-        print("Exception:", str(e))
-        mensaje = {
-            'error': str(e)
-        }        
         return {
             'statusCode': 500,
-            'body': mensaje
+            'body': json.dumps({'error': str(e)})
         }
